ebb65dbd9123d22a1a83d35db0be421d19ee09d8
LKimm <21102211@ittelkom-pwt.ac.id>
2024-03-07 22:34:30 +0700

    Praktikum2

---
+++ praktikum_2/bin/Dart Type System/Generic.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Contoh penggunaan List dengan tipe parameter generic
+  List<int> bilangan = [1, 2, 3, 4, 5];
+  List<String> kata = ['Informatika', 'Flutter', 'Pemrograman'];
+  List<dynamic> dynamicList = [1, 2, 3, 'empat']; // List<dynamic>
+
+  print('List bilangan: $bilangan');
+  print('List kata: $kata');
+  print('Dynamic List: $dynamicList');
+}
---
+++ praktikum_2/bin/Dart Type System/TypeInference.dart
@@ -0,0 +1,6 @@
+void main() {
+  // Contoh penggunaan type inference pada map
+  Map<String, dynamic> jurusan = {'prodi': 'Informatika', 'angkatan': 2020};
+
+  print('Map jurusan: $jurusan');
+}
