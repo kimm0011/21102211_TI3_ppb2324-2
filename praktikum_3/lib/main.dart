a9a146dd3d1eeb8c6280579586446cd7da98c758
LKimm <21102211@ittelkom-pwt.ac.id>
2024-03-08 08:44:50 +0700

    Praktikum3

--- praktikum_3/lib/main.dart
+++ praktikum_3/lib/main.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World'),
-        ),
-        body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text('Aplikasi Pertemuan 3'),
+          ),
+          body: Text(
+            "Hello, world!",
+            style: TextStyle(color: Colors.blueAccent, fontSize: 50),
+          )),
     );
   }
 }
